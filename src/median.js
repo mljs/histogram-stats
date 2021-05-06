f3b9590a9d3fa9c727ff9f65007a2b0e0e3a0ccf
WargnerM <wargner.moreno@correounivalle.edu.co>
2021-05-06 11:04:15 -0500

    feat: median function for a histogram is added

--- src/median.js
+++ src/median.js
@@ -1,5 +1,31 @@
-export function median(data) {
+/* 
+ * Returns the meadian x value from a histogram of sorted abscissa
+ * @param {object} data - x sorted abscissa by incresing order and y the number of occurrence of each x value
+ * @returns {number} xmedian - the median of x values 
+ */
+
+ export function median(data) {
   const { x, y } = data;
 
-  return 0;
+  if (x.length === 1) {
+    return x[0];
+  }
+
+  let sumY = 0;
+  let cumSumY = new Array(y.length);
+
+  for (let i = 0; i < y.length; i++) {
+    sumY += y[i];
+    cumSumY[i] = sumY;
+  }
+
+  for (let k = 0; k < cumSumY.length; k++) {
+    if (cumSumY[k] > sumY / 2) {
+      let xmedian = x[k];
+      return xmedian;
+    } else if (cumSumY[k] === sumY / 2) {
+      let xmedian = 0.5 * (x[k] + x[k + 1]);
+      return xmedian;
+    }
+  }
 }
