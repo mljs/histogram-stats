1eea7c5638542466b577c0c0003a98735329fa2c
WargnerM <wargner.moreno@correounivalle.edu.co>
2021-05-06 11:23:36 -0500

    feat: test cases added

--- src/__tests__/median.test.js
+++ src/__tests__/median.test.js
@@ -1,11 +1,39 @@
 import { median } from '../median.js';
 
 describe('median', () => {
-  it('should return 42', () => {
+  it('four elements having unit high, should return 2.5', () => {
     const data = {
       x: [1, 2, 3, 4],
-      y: [2, 2, 1, 1],
+    y: [1, 1, 1, 1],
     };
-    expect(median(data)).toBeCloseTo(13 / 6, 10);
+    expect(median(data)).toStrictEqual(2.5);
+  });
+  it('should return 2', () => {
+    const data = {
+      x: [1, 2, 3],
+      y: [1, 2, 1],
+    };
+    expect(median(data)).toStrictEqual(2);
+  });
+  it('should return 1', () => {
+    const data = {
+      x: [1, 2],
+      y: [3, 1],
+    };
+    expect(median(data)).toStrictEqual(1);
+  });
+  it('should return 2.5', () => {
+    const data = {
+      x: [1, 2, 3, 4],
+      y: [1, 2, 1, 2],
+    };
+    expect(median(data)).toStrictEqual(2.5);
+  });
+  it('should return 1.5', () => {
+    const data = {
+      x: [1, 2, 3],
+      y: [2, 1, 1],
+    };
+    expect(median(data)).toStrictEqual(1.5);
   });
 });
