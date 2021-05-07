a168864623b81f26728a0d9308d94d6817e51d39
WargnerM <wargner.moreno@correounivalle.edu.co>
2021-05-07 15:09:40 -0500

    feat: median function and its test cases added

--- src/median.js
+++ src/median.js
@@ -1,11 +1,15 @@
 /* 
- * Returns the meadian x value from a histogram of sorted abscissa
- * @param {object} data - x sorted abscissa by incresing order and y the number of occurrence of each x value
- * @returns {number} xmedian - the median of x values 
+ * Returns the median x value from a histogram
+ * @param {object} data - x sorted in increasing order and y the number of occurrence of each x value
+ * @returns {number} the median of x values
  */
 
  export function median(data) {
   const { x, y } = data;
+
+  if (x.length === 0) {
+    return NaN;
+  }
 
   if (x.length === 1) {
     return x[0];
@@ -13,19 +17,18 @@
 
   let sumY = 0;
   let cumSumY = new Array(y.length);
+  let i;
 
-  for (let i = 0; i < y.length; i++) {
+  for (i = 0; i < y.length; i++) {
     sumY += y[i];
     cumSumY[i] = sumY;
   }
 
-  for (let k = 0; k < cumSumY.length; k++) {
-    if (cumSumY[k] > sumY / 2) {
-      let xmedian = x[k];
-      return xmedian;
-    } else if (cumSumY[k] === sumY / 2) {
-      let xmedian = 0.5 * (x[k] + x[k + 1]);
-      return xmedian;
+  for (i = 0; i < cumSumY.length; i++) {
+    if (cumSumY[i] > sumY / 2) {
+      return x[i];
+    } else if (cumSumY[i] === sumY / 2) {
+      return 0.5 * (x[i] + x[i + 1]);
     }
   }
 }
